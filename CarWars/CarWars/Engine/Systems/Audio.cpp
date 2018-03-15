71e0b598209753fbe6d414f2f4517903d3ce7849
alphautka <alphautka@gmail.com>
2018-03-15 00:55:56 -0600

    base 3d audio, still need to implement movement of listener

--- CarWars/CarWars/Engine/Systems/Audio.cpp
+++ CarWars/CarWars/Engine/Systems/Audio.cpp
@@ -1,5 +1,5 @@
 #include "Audio.h"
-#include "../Entities/EntityManager.h"
+
 
 // Singleton
 Audio::Audio() { }
@@ -19,7 +19,7 @@ void Audio::Initialize() {
     FMOD::System_Create(&soundSystem);
     soundSystem->init(32, FMOD_INIT_NORMAL, 0);
     soundSystem->set3DSettings(1.0f, 1.0f, 1.0f); 
-    //soundSystem->set3DNumListeners(number of players...)
+    soundSystem->set3DNumListeners(4); // need to get number of players
 }
 
 void Audio::PlayAudio(const char *filename) {
@@ -32,9 +32,30 @@ void Audio::PlayAudio(const char *filename) {
         soundToPlay = sound;
     }
     soundSystem->playSound(soundToPlay, 0, false, &channel);
+
+    //FMOD_VECTOR pos = { 10.0f * 1.0f, 0.0f, 0.0f };
+    //FMOD_VECTOR vel = { 0.0f, 0.0f, 0.0f };
+    //soundSystem->playSound(sound1, 0, true, &channel2);
+    //channel2->set3DAttributes(&pos, &vel);
+    //channel2->setPaused(false); // sound starts paused
+}
+
+void Audio::PlayAudio(const char *filename, glm::vec3 position, glm::vec3 velocity) {
+    FMOD_VECTOR pos = { position.x, position.y, position.z };
+    FMOD_VECTOR vel = { velocity.x, velocity.y, velocity.z };
+
+    soundSystem->createSound(filename, FMOD_3D, 0, &sound1);
+    sound1->set3DMinMaxDistance(minDistance, maxDistance);
+    sound1->setMode(FMOD_LOOP_OFF);
+    soundSystem->playSound(sound1, 0, true, &channel2);
+    channel2->set3DAttributes(&pos, &vel);
+    channel2->setPaused(false);
 }
 
 void Audio::Update() { 
-
+    // fetch from physx
+    // iterate over number of players and adjsust listeners
+    //soundSystem->set3DListenerAttributes(0, &listenerpos, &vel, &forward, &up);
+    
     soundSystem->update();
 }
--- CarWars/CarWars/Engine/Systems/Audio.h
+++ CarWars/CarWars/Engine/Systems/Audio.h
@@ -3,6 +3,8 @@
 #include "System.h"
 #include "fmod/fmod.hpp"
 #include "fmod/fmod_errors.h"
+#include "../Entities/EntityManager.h"
+#include "glm/glm.hpp"
 
 typedef FMOD::Sound* SoundClass;
 
@@ -16,15 +18,19 @@ public:
 
     void Update() override;
     void PlayAudio(const char *filename);
+    void PlayAudio(const char *filename, glm::vec3 position, glm::vec3 velocity);
 
 private:
     FMOD::System *soundSystem;
     FMOD::Sound *sound, *soundToPlay;
     FMOD::Sound *sound1, *sound2;
-    FMOD::Channel *channel = 0;
+    FMOD::Channel *channel = 0, *channel2 = 0;
     FMOD_RESULT result;
     unsigned int version;
     int numsubsounds;
+
+    float minDistance = 0.5f;
+    float maxDistance = 5000.f;
 
     // No instantiation or copying
     Audio();
