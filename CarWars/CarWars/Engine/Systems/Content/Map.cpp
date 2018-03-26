a73a016ffdd0360b4cd05e72765ce9aad50fc19d
Blake Mealey <blakemealey@gmail.com>
2018-03-25 20:04:09 -0600

    Positioned heights of entities based on height map

--- CarWars/CarWars/Engine/Systems/Content/Map.cpp
+++ CarWars/CarWars/Engine/Systems/Content/Map.cpp
@@ -16,23 +16,30 @@ Map::Map(std::string dirPath) {
     // Load the map's scene
     ContentManager::DestroySceneAndLoadScene(data["Scene"]);
 
-    // Initialize mesh and collider for the height map
+    navigationMesh->UpdateMesh(EntityManager::GetComponents(ComponentType_RigidStatic));
+
+    // TODO: Initialize powerups and spawners from image
+
     if (heightMap) {
+        // Initialize entity heights based on height map
+        for (Entity* entity : EntityManager::GetChildren(EntityManager::GetRoot())) {
+            const float height = heightMap->GetHeight(entity->transform.GetLocalPosition());
+            entity->transform.Translate(glm::vec3(0.f, height, 0.f));
+        }
+
+        // Initialize height map collider
         Entity* floor = ContentManager::LoadEntity("Game/Floor.json");
         floor->GetComponent<RigidStaticComponent>()->AddCollider(new MeshCollider({
             { "CollisionGroup", "Ground" },
             { "HeightMap", dirPath }
         }));
 
+        // Initialize height map mesh
         EntityManager::AddComponent(floor, new MeshComponent({
             { "HeightMap", dirPath },
             { "Material", "Basic.json" },
             { "Texture", "Boulder.jpg" },
-            { "UvScale", {10, 10} }
+            { "UvScale",{ 10, 10 } }
         }));
     }
-
-    navigationMesh->UpdateMesh(EntityManager::GetComponents(ComponentType_RigidStatic));
-
-    // TODO: Initialize powerups and spawners from image
 }
